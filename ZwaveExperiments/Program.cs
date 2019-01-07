d7da526993f42122161a5728f5ebffa207f15d62
Julien Roncaglia <julien@roncaglia.fr>
2019-01-07 22:39:59 +0100

    First commit

---
+++ ZwaveExperiments/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ZwaveExperiments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
